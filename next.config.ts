ab8357687e342a6d550d9d8ec85dcaf9ad104e13
Open Tools Team <opencourse@example.com>
2026-02-10 13:18:23 +0900

    Remove COEP/COOP headers to allow ads

--- next.config.ts
+++ next.config.ts
@@ -7,23 +7,6 @@ import { NextConfig } from 'next';
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
     output: 'standalone',
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Cross-Origin-Embedder-Policy',
-                        value: 'require-corp',
-                    },
-                    {
-                        key: 'Cross-Origin-Opener-Policy',
-                        value: 'same-origin',
-                    },
-                ],
-            },
-        ];
-    },
     webpack: (config: any, { isServer }: { isServer: boolean }) => {
         if (!isServer) {
             config.resolve.fallback = {
